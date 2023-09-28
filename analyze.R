dd9cd1fa4debea8a833651415d058d5846d48111
David J. H. Shih <djh.shih@gmail.com>
2023-09-28 17:55:27 +0800

    Initial commit.

---
+++ analyze.R
@@ -0,0 +1,146 @@
+# Analyze filtered ata
+
+library(io)
+library(org.Hs.eg.db)
+library(ggplot2)
+library(dplyr)
+
+# for production use
+#library(array2rnaseq)
+
+# for development only
+library(devtools)
+load_all("~/projects/r/array2rnaseq")
+
+probes <- qread("annot/probes.rds");
+rseq
+
+rseqt.f <- rseqt[rownames(rseqt) %in% probes.f$genes, ]
+marrt.f <- marrt[rownames(marrt) %in% probes.f$genes, ]
+
+# Linear regression
+## Linear model
+lm <- lapply(probes.f$genes, function(g) {
+  lm(rseqt.f[g, ] ~ marrt.f[g, ])
+})
+summary_lst <- lapply(lm, summary)
+
+## linear property for each probes
+rse <- r.2 <- adj.r.2 <- coef <- c()
+for (i in 1:dim(probes.f)[1]) {
+  rse <- append(rse, summary_lst[[i]]$sigma) # Residual standard error
+  r.2 <- append(r.2, summary_lst[[i]]$r.squared)
+  adj.r.2 <- append(adj.r.2, summary_lst[[i]]$adj.r.squared)
+}
+probes.f <- cbind(probes.f, rse = rse, r.2 = r.2, adj.r.2 = adj.r.2)
+hist(r.2, breaks = 100)
+
+
+# Linear functional probes classified by R^2
+# 237 probes follow linear model
+curve.cond <- r.2 < 0.95
+probes.f.lin <- subset(probes.f, !curve.cond)
+curve_idx <- which(curve.cond)
+rseqt.f.lin <- rseqt.f[-curve_idx, ]
+marrt.f.lin <- marrt.f[-curve_idx, ]
+func.lin <- lm[rownames(rseqt.f.lin)]
+dim(rseqt.f.lin)[1] # 237
+length(func.lin) # 237
+
+
+
+# --- linear model ---
+
+# Implement linear model for all genes
+lin.pred <- lapply(1:dim(marrt.f.lin)[1], function(i) {
+  linear(i, X = marrt.f.lin, Y = rseqt.f.lin, level = 0.95)
+  }
+)
+
+
+# Calculate fev for all gene based on linear model
+fev.lin <- unlist(lapply(1:length(lin.pred), function(i) {fev.func(rseqt.f.lin[i, ], lin.pred[[i]]$fit)}))
+
+
+# Scatter plots of multiple genes fitted on the linear model
+scatter.s(X = marrt.f.lin, Y = rseqt.f.lin, probes = probes.f.lin, pred = lin.pred,
+               folder = "linear_plots", num.pic = 5, w = NULL,
+               level = 0.95, label = FALSE, press = TRUE, view = TRUE
+)
+# num.pic: num.pic pictures drawn
+# label: whether to named the picture(Y/N/S/HS/C)
+# view: TRUE/FALSE (whether to view pic)
+# press: TRUE/FALSE(whether see pic one by one )
+
+
+
+# --- weighted least square ---
+
+# Implement SCAM model for all genes
+wlin.pred <- lapply(1:dim(marrt.f.lin)[1], function(i) {
+  linear(i, X = marrt.f.lin, Y = rseqt.f.lin, level = 0.95, w = NULL)
+}
+)
+
+
+# Calculate fev for all gene based on SCAM model
+fev.lin <- unlist(lapply(1:length(wlin.pred), function(i) {fev.func(rseqt.f.lin[i, ], wlin.pred[[i]]$fit)}))
+
+
+# Scatter plots of multiple genes fitted on the weighted linear model
+scatter.s(X = marrt.f.lin, Y = rseqt.f.lin, probes = probes.f.lin, pred = wlin.pred,
+               folder = "weighted_linear_plots", num.pic = 5, w = NULL,
+               level = 0.95, label = FALSE, press = TRUE, view = TRUE
+)
+
+
+
+# --- SCAM model ---
+
+# Nonlinear probes
+probes.f.cur <- subset(probes.f, curve.cond)
+rseqt.f.cur <- rseqt.f[curve_idx, ]
+marrt.f.cur <- marrt.f[curve_idx, ]
+dim(marrt.f.cur) # 10298   294
+
+
+# Implement SCAM model for all genes
+k.s <- c(seq(4, 20, 2))
+scam.pred <- lapply(1:dim(marrt.f.cur)[1], function(i) {
+  SCAM.model(i, k.s, X = marrt.f.cur, Y = rseqt.f.cur, level = 0.95)
+  #print(i)
+  }
+)
+
+
+# Calculate fev of scam model
+fev.scam <- unlist(lapply(1:dim(marrt.f.cur)[1], function(i) 
+  {fev.func(rseqt.f.cur[i, ], scam.pred[[i]]$fit)}
+  )
+)
+hist(fev.scam, breaks = 100)
+
+
+
+# Scatter plots of multiple genes fitted on SCAM model
+scatter.s(X = marrt.f.cur, Y = rseqt.f.cur, probes = probes.f.cur, pred = scam.pred,
+          folder = "scam_plots", num.pic = 5, w = NULL,
+          level = 0.95, label = FALSE, press = TRUE, view = TRUE
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# TODO
+# 1. check the edf of non-conver probes, see does it equal to 1;
+
+
---
+++ data/get.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Retrieve data
+
+set -euo pipefail
+
+# TODO (with help from Noel)
+
+

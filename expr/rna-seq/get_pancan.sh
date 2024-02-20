b472a184b4fb5c6f9e1390543ba2e9c63460d50c
ncyue2 <ncyue2@connect.hku.hk>
2024-02-20 16:29:24 +0800

    Add file.

---
+++ expr/rna-seq/get_pancan.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# download rna-seq data and annotation
+
+set -euo pipefail
+
+# download exprs
+gdc-client download 3586c0da-64d0-4b74-a449-5ff4d9136611
+
+mv ./3586c0da-64d0-4b74-a449-5ff4d9136611/EBPlusPlusAdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.tsv ./pancan_exprs.tsv
+
+rm -rf 3586c0da-64d0-4b74-a449-5ff4d9136611
+
+
+# download annotation
+gdc-client download 1b5f413e-a8d1-4d10-92eb-7c4ae739ed81
+
+mv ./1b5f413e-a8d1-4d10-92eb-7c4ae739ed81/TCGA-CDR-SupplementalTableS1.xlsx ./pancan_pheno.xlsx
+
+# remove the directory
+rm -rf 1b5f413e-a8d1-4d10-92eb-7c4ae739ed81 
---
+++ expr/rna-seq/preprocess.R
@@ -0,0 +1,58 @@
+# preprocess and match the exprs to the phenotype annotation
+
+library(io)
+library(openxlsx)
+
+exprs_df <- qread("./pancan_exprs.tsv");
+pheno_df <- read.xlsx("./pancan_pheno.xlsx", rowNames = TRUE);
+
+dim(exprs_df) # 20531 11070
+dim(pheno_df) # 11160    33
+
+exprs_df[1:5, 1:5]
+# keep the entrez ids
+gene_ids <- exprs_df$gene_id;
+entrez_ids <- unlist(lapply(gene_ids, function(x){
+  strsplit(x, "\\|")[[1]][2]
+}))
+
+rownames(exprs_df) <- entrez_ids;
+exprs_df <- exprs_df[, !colnames(exprs_df) == "gene_id"]
+
+# match samples
+exprs_sample_ids <- colnames(exprs_df);
+exprs_sample_ids <- substr(exprs_sample_ids, 1, 12); # to match the format
+
+# create a reference table
+exprs_sample_ids_df <- data.frame(
+  "full_id" = colnames(exprs_df),
+  "short_id" = exprs_sample_ids
+)
+exprs_sample_ids_df[1:5,]
+qwrite(exprs_sample_ids_df, "patient_barcode_ref.tsv");
+
+# replace the full column names with the trimmed column names
+colnames(exprs_df) <- exprs_sample_ids;
+
+intersect_samples <- intersect(exprs_sample_ids, pheno_df$bcr_patient_barcode);
+length(intersect_samples) # 10230
+
+intersect_samples[1:5]
+
+# match
+pheno_df <- pheno_df[match(intersect_samples, pheno_df$bcr_patient_barcode), ]
+dim(pheno_df) # 10230    33
+
+exprs_df <- exprs_df[, match(intersect_samples, colnames(exprs_df))]
+dim(exprs_df) # 20531 10230
+
+stopifnot(colnames(exprs_df) == pheno_df$bcr_patient_barcode)
+
+exprs_df[1:5, 1:5]
+pheno_df[1:5, 1:5]
+
+qwrite(
+  data.frame("gene_id" = rownames(exprs_df), exprs_df), 
+  "exprs_df-matched.tsv"
+);
+qwrite(pheno_df, "pheno_df-matched.tsv");

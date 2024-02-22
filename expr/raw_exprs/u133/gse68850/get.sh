f60149131eb8c8fbe10557bbecd18ab71e17b280
ncyue2 <ncyue2@connect.hku.hk>
2024-02-22 16:32:26 +0800

    Add file.

---
+++ expr/raw_exprs/u133/gse68850/get.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# code modified from:
+# https://github.com/djhshih/analysis-xpa-geo/blob/master/GSE70818/get.sh
+# download raw .CEL files from GEO, accession number is gse68850
+
+set -euo pipefail
+IFS=$'\n\t'
+
+accession=GSE68850
+outdir_name="./raw/"
+
+url=ftp://ftp.ncbi.nlm.nih.gov/geo/series
+outdir=raw
+
+mkdir -p $outdir
+cd $outdir
+
+# download and extract main raw archive file
+group=${accession%???}nnn
+file=${accession}_RAW.tar
+curl -o ${file} ${url}/${group}/${accession}/suppl/$file
+tar -xf ${file} && rm ${file}
+gunzip *.gz
+
+# remove all the .txt files
+rm -rf *.txt
+
+# create tsv table with all directory
+ls -I "*.tsv" | awk -v path="$outdir_name" '{print path $0}' > ../raw_lst.tsv
+
+cd -
+
+sed -i '1i files' raw_lst.tsv
+
+# get pheno data
+#Rscript ./download_pheno.R
+
+# Run frma
+#Rscript run_frma.R
+

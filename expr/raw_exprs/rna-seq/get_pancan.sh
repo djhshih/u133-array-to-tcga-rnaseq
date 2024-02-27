dafd6d7fe15b17983df12c1ad199f0814cb7f282
ncyue2 <ncyue2@connect.hku.hk>
2024-02-27 10:19:57 +0800

    Comment out phenotype associated lines.

--- expr/raw_exprs/rna-seq/get_pancan.sh
+++ expr/raw_exprs/rna-seq/get_pancan.sh
@@ -12,12 +12,12 @@ rm -rf 3586c0da-64d0-4b74-a449-5ff4d9136611
 
 
 # download annotation
-gdc-client download 1b5f413e-a8d1-4d10-92eb-7c4ae739ed81
+#gdc-client download 1b5f413e-a8d1-4d10-92eb-7c4ae739ed81
 
-mv ./1b5f413e-a8d1-4d10-92eb-7c4ae739ed81/TCGA-CDR-SupplementalTableS1.xlsx ./pancan_pheno.xlsx
+#mv ./1b5f413e-a8d1-4d10-92eb-7c4ae739ed81/TCGA-CDR-SupplementalTableS1.xlsx ./pancan_pheno.xlsx
 
 # remove the directory
-rm -rf 1b5f413e-a8d1-4d10-92eb-7c4ae739ed81 
+#rm -rf 1b5f413e-a8d1-4d10-92eb-7c4ae739ed81 
 
 # preprocess the data
-Rscript preprocess.R
+#Rscript preprocess.R

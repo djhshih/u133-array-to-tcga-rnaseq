b5bf90eb9718fcef38cba17732c71d63761932fa
ncyue2 <ncyue2@connect.hku.hk>
2024-02-27 12:24:34 +0800

    Add in preprocess step.

--- expr/raw_exprs/rna-seq/get_pancan.sh
+++ expr/raw_exprs/rna-seq/get_pancan.sh
@@ -9,3 +9,6 @@ gdc-client download 3586c0da-64d0-4b74-a449-5ff4d9136611
 mv ./3586c0da-64d0-4b74-a449-5ff4d9136611/EBPlusPlusAdjustPANCAN_IlluminaHiSeq_RNASeqV2.geneExp.tsv ./pancan_exprs.tsv
 
 rm -rf 3586c0da-64d0-4b74-a449-5ff4d9136611
+
+# preprocess
+Rscript preprocess.R
